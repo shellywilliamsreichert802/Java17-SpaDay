e12c1ba9c6418615048308ec77901885b397e1fe
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-05-09 20:08:51 -0500

    minor change

--- src/main/java/org/launchcode/SpaDayApplication.java
+++ src/main/java/org/launchcode/SpaDayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpaDayApplication {
-
+//something minor
 	public static void main(String[] args) {
 		SpringApplication.run(SpaDayApplication.class, args);
 	}
